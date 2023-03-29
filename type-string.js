74129ae752692e20ffd597182736cfd09c848a42
ALINE PINHELLI <97640506+AlinePinhelli@users.noreply.github.com>
2023-03-29 19:30:58 -0300

    Exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,23 @@
+const texto1= "Olá, mundo!";
+const texto2= 'Olá,mundo!';
+const senha= "senhaSuperSegura456!";
+const StringDeNumeros= "34567";
+const citacao= "Meu nome é";
+const meuNome= " Aline";
+
+console.log(citacao + meuNome)
+
+//concatenação (+)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const senhaDois = "minhaSenha123";
+console.log(senhaDois.length) // 13 caracteres
